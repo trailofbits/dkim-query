b0944f475f92e913f6f99f4a8325b30706aaf98b
Postmodern <postmodern.mod3@gmail.com>
2014-11-26 14:31:33 -0800

    Alias the fields to human readable names.

--- lib/dkim_parse/key.rb
+++ lib/dkim_parse/key.rb
@@ -4,20 +4,28 @@ module DKIMParse
   class Key
 
     attr_reader :v
+    alias v version
 
     attr_reader :g
+    alias g granularity
 
     attr_reader :h
+    alias h hash
 
     attr_reader :k
+    alias k key
 
     attr_reader :n
+    alias n notes
 
     attr_reader :p
+    alias p public_key
 
     attr_reader :s
+    alias s service_type
 
     attr_reader :t
+    alias t flags
 
     #
     # Initialize the key.
